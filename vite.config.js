8a6f48c33db72f1d86cc1cc0f9db047e869dd396
cargoer <topdon.chen@gmail.com>
2025-07-18 15:21:42 +0800

    githubPage

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,26 @@ import path from 'path'
 import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(), 
-    svgLoader({
-      defaultImport: 'url',
-    }),
-  ],
-  base: './',
-  server: {
-    open: true, // 自动打开浏览器
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src') // 将 @ 指向 src 目录
+export default defineConfig(({ mode }) => {
+  // 根据环境变量动态设置 base
+  const base = mode === 'production' 
+    ? '/metro-app/'      // 生产环境使用 GitHub Pages 子路径
+    : '/' 
+  return {
+    plugins: [
+      vue(), 
+      svgLoader({
+        defaultImport: 'url',
+      }),
+    ],
+    base,
+    server: {
+      open: true, // 自动打开浏览器
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src') // 将 @ 指向 src 目录
+      }
     }
   }
 })
